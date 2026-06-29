82d0318d449a926d4f87a954e100c967eb530588
Raamnath A <raamnathannamalai@gmail.com>
2026-06-29 21:12:22 +0530

    Add background queue worker to start script

--- start.sh
+++ start.sh
@@ -11,5 +11,8 @@ php artisan migrate --force
 # Seed database with default data (idempotent, safe to run multiple times)
 php artisan db:seed --force
 
+# Start Laravel queue worker in the background (processes notifications/emails without blocking)
+php artisan queue:work --tries=3 --timeout=90 &
+
 # Start the Apache web server
 apache2-foreground
